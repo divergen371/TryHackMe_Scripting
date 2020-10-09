b0c769ea92af6941f0d2ba9f3ff0bf02eb6bbeff
divergen371 <divergen@protonmail.com>
2020-10-10 00:13:58 +0900

    update

--- client.py
+++ client.py
@@ -3,13 +3,81 @@ import time
 import sys
 import re
 
-def Main():
-    serverIP   = sys.argv[1]
-    serverPort = 1337
-    startNum = 0
 
-    while serverPort != 9765:
+def main():
+    server_ip = sys.argv[1]
+    server_port = 1337
+    old_num = 0
+
+    while server_port != 9765:
         try:
-            if serverPort == 1337:
-                print(f"Connecting to {serverIP} waiting for port {serverPort} to become available.")
+            if server_port == 1337:
+                print(
+                    f"Connecting to {server_ip} waiting for port {server_port} to become available."
+                )
 
+            s = socket.socket()
+            s.connect((server_ip, server_port))
+
+            get_req = f"GET / HTTP/1.0\r\nHost: {server_ip}:{server_port}\r\n\r\n"
+            s.send(get_req.encode("utf-8"))
+
+            while True:
+                response = s.recv(1024)
+                if len(response) < 1:
+                    break
+
+                data = response.decode("utf-8")
+
+            op, new_num, next_port = assign_data(data)
+            old_num = do_math(op, old_num, new_num)
+            print(f"Current number is {old_num}, moving onto Port {next_port}")
+
+            server_port = next_port
+
+            s.close()
+
+        except:
+            s.close()
+            time.sleep(3)
+            pass
+
+    print(f"The final answer is {round(old_num,2)}")
+
+
+def do_math(op, old_num, new_num):
+    """
+
+    :param op: str
+    :param old_num: int
+    :param new_num: int
+    :return: int, None
+    """
+    if op == "add":
+        return old_num + new_num
+    elif op == "minus":
+        return old_num - new_num
+    elif op == "divide":
+        return old_num / new_num
+    elif op == "multiply":
+        return old_num * new_num
+    else:
+        return None
+
+
+def assign_data(data: str):
+    """
+
+    :param data: str
+    :return: str, int
+    """
+    data_arr = re.split(" |\*|\n", data)
+    data_arr = list(filter(None, data_arr))
+    op = data_arr[-3]
+    new_num = float(data_arr[-2])
+    next_port = int(data_arr[-1])
+    return op, new_num, next_port
+
+
+if __name__ == "__main__":
+    main()
